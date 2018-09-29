064d6f11ad5ec41b8bf180207dda26c90ebb8318
starsoftdev <26078620+starsoftdev@users.noreply.github.com>
2018-09-29 21:53:43 +0900

    update PostOffer page component

--- app/containers/Offer/sagas.js
+++ app/containers/Offer/sagas.js
@@ -3,7 +3,14 @@
 // Rules on how to organize this file: https://github.com/erikras/ducks-modular-redux
 
 import { fromJS } from 'immutable';
-import { call, put, takeLatest, select, all } from 'redux-saga/effects';
+import {
+  call,
+  put,
+  takeLatest,
+  takeEvery,
+  select,
+  all,
+} from 'redux-saga/effects';
 import axios from 'axios';
 import { API_URL, REQUESTED, SUCCEDED, FAILED, ERROR } from 'enum/constants';
 import type { Action, State } from 'types/common';
@@ -14,8 +21,9 @@ import { getToken, getUserId } from 'containers/App/selectors';
 // ------------------------------------
 // Constants
 // ------------------------------------
-const CREATE_OFFER = 'Acheev/App/CREATE_OFFER';
-
+const CREATE_OFFER = 'Acheev/Offer/CREATE_OFFER';
+const UPLOAD_OFFER_PHOTOS = 'Acheev/Offer/UPLOAD_REVIEW_PHOTOS';
+const REMOVE_OFFER_PHOTO = 'Acheev/Offer/REMOVE_REVIEW_PHOTO';
 // ------------------------------------
 // Actions
 // ------------------------------------
@@ -36,18 +44,45 @@ const createOfferRequestError = error => ({
   payload: error,
 });
 
+export const requestOfferPhotoUpload = (index: number, payload: string) => ({
+  type: UPLOAD_OFFER_PHOTOS + REQUESTED,
+  payload,
+  meta: {
+    index,
+  },
+});
+const offerPhotoUploadSuccess = (index: number, payload: Object) => ({
+  type: UPLOAD_OFFER_PHOTOS + SUCCEDED,
+  payload,
+  meta: {
+    index,
+  },
+});
+const offerPhotoUploadError = (index, error) => ({
+  type: UPLOAD_OFFER_PHOTOS + ERROR,
+  payload: error,
+  meta: {
+    index,
+  },
+});
+
 // ------------------------------------
 // Reducer
 // ------------------------------------
 const initialState = fromJS({
   isLoading: false,
   error: '',
+  uploadedPhotos: [],
+  isUploading: false,
+  uploadError: '',
 });
 
 export const reducer = (
   state: State = initialState,
-  { type, payload }: Action
+  { type, payload, meta }: Action
 ) => {
+  let uploadedPhotos = state.get('uploadedPhotos');
+  let isUploading = false;
   switch (type) {
     case CREATE_OFFER + REQUESTED:
       return state.set('isLoading', true);
@@ -64,6 +99,53 @@ export const reducer = (
         `Something went wrong.
         Please try again later or contact support and provide the following error information: ${payload}`
       );
+
+    case UPLOAD_OFFER_PHOTOS + REQUESTED:
+      uploadedPhotos = uploadedPhotos.set(
+        meta.index,
+        fromJS({ isLoading: true, link: '', error: null })
+      );
+      return state
+        .set('isUploading', true)
+        .set('uploadedPhotos', uploadedPhotos)
+        .set('uploadError', null);
+
+    case UPLOAD_OFFER_PHOTOS + SUCCEDED:
+      uploadedPhotos = uploadedPhotos.set(
+        meta.index,
+        fromJS({
+          isLoading: false,
+          link: payload.link,
+          error: null,
+        })
+      );
+      uploadedPhotos.forEach(v => {
+        isUploading = isUploading || v.get('isLoading', false);
+      });
+      return state
+        .set('isUploading', isUploading)
+        .set('uploadedPhotos', uploadedPhotos)
+        .set('uploadError', '');
+
+    case UPLOAD_OFFER_PHOTOS + ERROR:
+      uploadedPhotos = uploadedPhotos.set(
+        meta.index,
+        fromJS({
+          isLoading: false,
+          link: '',
+          error: 'error',
+        })
+      );
+      uploadedPhotos.forEach(v => {
+        isUploading = isUploading || v.get('isLoading', false);
+      });
+      return state
+        .set('isUploading', false)
+        .set('uploadedPhotos', uploadedPhotos)
+        .set(
+          'uploadError',
+          `Something went wrong. Please try again later or contact support and provide the following error information: ${payload}`
+        );
 
     default:
       return state;
@@ -97,6 +179,31 @@ function* CreateOfferRequest({ payload }) {
   }
 }
 
+function* UploadOfferPhotoRequest({ payload, meta: { index } }) {
+  const token = yield select(getToken);
+  const userId = yield select(getUserId);
+  try {
+    const response = yield call(axios, {
+      method: 'POST',
+      url: `${API_URL}/offer/user/${userId}/thumbnails`,
+      data: {
+        thumbnails: payload,
+      },
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    if (response.status === 200) {
+      yield put(offerPhotoUploadSuccess(index, response.data));
+    } else {
+      yield put(offerPhotoUploadError(index, response.data.message));
+    }
+  } catch (error) {
+    yield put(offerPhotoUploadError(index, error));
+  }
+}
+
 export default function*(): Saga<void> {
-  yield all([takeLatest(CREATE_OFFER + REQUESTED, CreateOfferRequest)]);
+  yield all([
+    takeLatest(CREATE_OFFER + REQUESTED, CreateOfferRequest),
+    takeEvery(UPLOAD_OFFER_PHOTOS + REQUESTED, UploadOfferPhotoRequest),
+  ]);
 }
--- app/pages/PostOffer/index.js
+++ app/pages/PostOffer/index.js
@@ -5,17 +5,24 @@ import Form, { Field } from 'react-formal';
 import yup from 'yup';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
+import { List } from 'immutable';
 import { convertToRaw } from 'draft-js';
 import draftToHtml from 'draftjs-to-html';
 
 import injectSagas from 'utils/injectSagas';
-import saga, { reducer, requestCreateOffer } from 'containers/Offer/sagas';
+import saga, {
+  reducer,
+  requestCreateOffer,
+  requestOfferPhotoUpload,
+} from 'containers/Offer/sagas';
 
 import PageBanner from 'components/PageBanner';
 import TextAreaField from 'components/TextAreaField';
 import Typeahead from 'components/Typeahead';
 import Button from 'components/Button';
 import Editor from 'components/Editor';
+import Dropzone from 'components/Dropzone';
+import Link from 'components/Link';
 import ValidationMessage from 'components/ValidationMessage';
 
 import FILTER_OPTIONS from 'enum/filter/options';
@@ -37,8 +44,11 @@ const schema = yup.object({
 
 type Props = {
   requestCreateOffer: Function,
+  uploadPhoto: Function,
   isLoading: boolean,
   error: string,
+  uploadedPhotos: List<Map>,
+  isUploading: boolean,
 };
 
 type State = {
@@ -51,6 +61,7 @@ type State = {
     opening_message: string,
   },
   editorState: ?Object,
+  uploadedFiles: Array<Object>,
 };
 
 class PostOfferPage extends Component<Props, State> {
@@ -62,6 +73,7 @@ class PostOfferPage extends Component<Props, State> {
       opening_message: '',
     },
     editorState: null,
+    uploadedFiles: [],
   };
   componentDidUpdate(prevProps: Props) {
     const { isLoading, error } = this.props;
@@ -74,6 +86,7 @@ class PostOfferPage extends Component<Props, State> {
           opening_message: '',
         },
         editorState: null,
+        uploadedFiles: [],
       });
     }
   }
@@ -86,6 +99,36 @@ class PostOfferPage extends Component<Props, State> {
       editorState,
     });
   };
+  onDrop = (accepted: Array<Object>) => {
+    // eslint-disable-next-line
+    console.log('*********', accepted);
+    // const uploadedFiles = this.state.uploadedFiles;
+    // const photosCount = uploadedFiles.length;
+    // const newState = cloneDeep(this.state);
+    // newState.uploadedFiles = uploadedFiles.concat(accepted);
+    const photosCount = 0;
+    for (let index = 0; index < accepted.length; index += 1) {
+      //const gIndex = index + photosCount;
+      var reader = new FileReader();
+
+      // Closure to capture the file information.
+      reader.onload = e => {
+        console.log(e);
+        this.props.uploadPhoto(0, e.target.result);
+      };
+
+      // Read in the image file as a data URL.
+      reader.readAsBinaryString(accepted[index]);
+
+      //newState.uploadedFiles[gIndex].progress = 0;
+    }
+    //this.setState(newState);
+  };
+  triggerFileDialog = () => {
+    this.refDiv.click();
+  };
+  dropzoneRef: ?Object;
+  refDiv: HTMLElement;
   render() {
     return (
       <div className="postOfferPage">
@@ -96,7 +139,7 @@ class PostOfferPage extends Component<Props, State> {
           onChange={model => this.setState({ model })}
           onSubmit={e => this.props.requestCreateOffer(e)}
         >
-          <div className="row mb-hg">
+          <div className="row mb-xl">
             <div className="column large-8 large-offset-2">
               <div className="row align-middle mb-lg">
                 <div className="column shrink">
@@ -180,7 +223,7 @@ class PostOfferPage extends Component<Props, State> {
               </div>
             </div>
           </div>
-          <div className="row mb-hg">
+          <div className="row mb-xl">
             <div className="column large-8 large-offset-2 np">
               <div className="row align-middle mb-lg">
                 <div className="column shrink">
@@ -195,7 +238,68 @@ class PostOfferPage extends Component<Props, State> {
               <ValidationMessage for="description" />
             </div>
           </div>
-          <div className="row mb-hg">
+          <div className="row mb-xl">
+            <div className="column large-8 large-offset-2 np">
+              <div className="row align-middle mb-lg">
+                <div className="column shrink">
+                  <h1 className="postOfferPage__label ml-mn">Gallery</h1>
+                </div>
+              </div>
+              <div className="row">
+                <div className="column">
+                  <div className="postOfferPage__uploader">
+                    <div className="postOfferPage__uploaderToolbar row align-middle nm">
+                      <div className="column">
+                        <Link
+                          className="postOfferPage__btnUpload"
+                          onClick={this.triggerFileDialog}
+                        >
+                          + Upload Images
+                        </Link>
+                      </div>
+                      <div className="column shrink text-right npr">
+                        <Link className="postOfferPage__uploaderToolbarButton">
+                          Set as featured image
+                        </Link>
+                        <Link className="postOfferPage__uploaderToolbarButton">
+                          Select
+                        </Link>
+                        <Link className="postOfferPage__uploaderToolbarButton">
+                          Select All
+                        </Link>
+                        <Link className="postOfferPage__uploaderToolbarButton">
+                          Remove
+                        </Link>
+                      </div>
+                    </div>
+                    <Dropzone
+                      className="postOfferPage__dropzone"
+                      ref={node => {
+                        this.dropzoneRef = node;
+                      }}
+                      accept="image/*"
+                      onDrop={this.onDrop}
+                    >
+                      <div className="text-center">
+                        <p className="postOfferPage__labelDesc mb-sm">
+                          Drag your image here
+                        </p>
+                        <p
+                          className="postOfferPage__labelDesc"
+                          ref={node => {
+                            this.refDiv = node;
+                          }}
+                        >
+                          and Select one picture for your featured image
+                        </p>
+                      </div>
+                    </Dropzone>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row mb-xl">
             <div className="column large-8 large-offset-2 np">
               <div className="row align-middle mb-lg">
                 <div className="column shrink">
@@ -244,10 +348,14 @@ class PostOfferPage extends Component<Props, State> {
 const mapStateToProps = state => ({
   isLoading: state.getIn(['offer', 'isLoading']),
   error: state.getIn(['offer', 'error']),
+  uploadedPhotos: state.getIn(['offer', 'uploadedPhotos']),
+  isUploading: state.getIn(['offer', 'isUploading']),
 });
 
 const mapDispatchToProps = dispatch => ({
   requestCreateOffer: payload => dispatch(requestCreateOffer(payload)),
+  uploadPhoto: (index, payload) =>
+    dispatch(requestOfferPhotoUpload(index, payload)),
 });
 
 export default compose(
