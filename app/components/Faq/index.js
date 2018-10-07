5405737657e1397997220dd58bb90f24dc1f1878
starsoftdev <26078620+starsoftdev@users.noreply.github.com>
2018-10-08 02:16:37 +0900

    update Faq component

--- app/components/Faq/index.js
+++ app/components/Faq/index.js
@@ -21,39 +21,6 @@ type State = {
   activeKey: string,
 };
 
-const data = fromJS([
-  {
-    question: `What happens after I purchase this gig? How long to get started?`,
-    answer: `You will be sent an onboarding profile questionnaire(s) to complete 2.
-      You will be asked to share user-names and logins 3. It takes 5 business days to get started 4.
-      We prefer to work long term with clients to get a smashing ROI`,
-  },
-  {
-    question: `Do you offer a discount if I want more than 1 month? Sure do!`,
-    answer: `You will be sent an onboarding profile questionnaire(s) to complete 2.
-      You will be asked to share user-names and logins 3. It takes 5 business days to get started 4.
-      We prefer to work long term with clients to get a smashing ROI`,
-  },
-  {
-    question: `What’s included in your social media posts?`,
-    answer: `You will be sent an onboarding profile questionnaire(s) to complete 2.
-      You will be asked to share user-names and logins 3. It takes 5 business days to get started 4.
-      We prefer to work long term with clients to get a smashing ROI`,
-  },
-  {
-    question: `What is in the 3 month premium gig? Glad you asked :)`,
-    answer: `You will be sent an onboarding profile questionnaire(s) to complete 2.
-      You will be asked to share user-names and logins 3. It takes 5 business days to get started 4.
-      We prefer to work long term with clients to get a smashing ROI`,
-  },
-  {
-    question: `I have more questions?`,
-    answer: `You will be sent an onboarding profile questionnaire(s) to complete 2.
-      You will be asked to share user-names and logins 3. It takes 5 business days to get started 4.
-      We prefer to work long term with clients to get a smashing ROI`,
-  },
-]);
-
 class Faq extends React.Component<Props, State> {
   state = {
     activeKey: '1',
@@ -74,7 +41,7 @@ class Faq extends React.Component<Props, State> {
         <div className="faq__titleWrapper">
           <h1 className="faq__title">Frequently Asked Questions</h1>
         </div>
-        {data.map((item, key) => {
+        {questions.map((item, key) => {
           const question = item.get('question', '');
           const a = item.get('answer');
           const answers = typeof a === 'string' ? fromJS([a]) : a;
