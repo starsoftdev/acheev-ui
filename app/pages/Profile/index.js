aa1a9b3f68b801c350a84074c2611d9c4efd2b7f
starsoftdev <26078620+starsoftdev@users.noreply.github.com>
2018-10-08 11:15:09 +0900

    add banner to profile page

--- app/pages/Profile/index.js
+++ app/pages/Profile/index.js
@@ -7,16 +7,34 @@ import Routes from './routes';
 import './styles.scss';
 
 type Props = {
+  user: Object,
   match: Object,
 };
 
 class Profile extends Component<Props> {
   render() {
     const {
+      user,
       match: { url },
     } = this.props;
+    const username =
+      user.get('first_name') && user.get('last_name')
+        ? `${user.get('first_name')} ${user.get('last_name')}`
+        : user.get('username');
+    const userImg = user.get('image') ? user.getIn(['image', 'src']) : '';
     return (
       <div className="profile">
+        <div className="profile__banner">
+          <div className="row align-center">
+            <div className="column shrink">
+              <div
+                className="profile__bannerImg mb-md"
+                style={{ backgroundImage: `url(${userImg})` }}
+              />
+              <h1 className="profile__bannerUsername">{username}</h1>
+            </div>
+          </div>
+        </div>
         <Routes url={url} />
       </div>
     );
