a7cfcddb724d9805a919b6401c3b7b86e1ece8c3
starsoftdev <26078620+starsoftdev@users.noreply.github.com>
2018-10-15 04:20:37 +0900

    create dashboard page component

---
+++ app/pages/Dashboard/index.js
@@ -0,0 +1,39 @@
+// @flow
+
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import UserInfoCard from 'components/UserInfoCard';
+
+import './styles.scss';
+
+type Props = {
+  currentUser: Object,
+};
+
+class Dashboard extends Component<Props> {
+  render() {
+    const { currentUser } = this.props;
+    return (
+      <div className="dashboard">
+        <div className="row">
+          <div className="column small-12 large-4">
+            <div className="row column mb-lg">
+              <UserInfoCard user={currentUser} />
+            </div>
+          </div>
+          <div className="column small-12 large-8" />
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  currentUser: state.getIn(['app', 'user']),
+});
+
+export default connect(
+  mapStateToProps,
+  null
+)(Dashboard);
